5ac2708016cb59bdb4fb7350040dca72d180b511
ravi-rauniyar <153609267+ravi-rauniyar@users.noreply.github.com>
2025-03-26 21:53:46 +0545

    refact: version one app styling completed (#3)

--- js/ui.js
+++ js/ui.js
@@ -10,8 +10,7 @@ function renderTasks() {
     if (tasks.length)
         tasks.forEach((task, index) => {
             const li = document.createElement('li');
-            li.innerHTML = `<a href="details.html" onclick="viewTask(${index})">${task.name}</a>
-                            <button onclick="deleteTask(${index})">Delete</button>`;
+            li.innerHTML = `<a href="details.html" onclick="viewTask(${index})">${task.name}</a><button onclick="deleteTask(${index})"><img src="../assets/delete.svg"/></button>`;
             taskList.appendChild(li);
         });
     else {
